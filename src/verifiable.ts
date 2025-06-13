03100cb25b823e4e6fcadb48bffc3f3d27a989a5
k4yr2 <kyrzkaya@gmail.com>
2025-06-13 20:28:48 +0300

    Add VerifiableWrapper class to enhance verification functionality and type handling

--- src/verifiable.ts
+++ src/verifiable.ts
@@ -1,5 +1,21 @@
-import Loadable from "./loadable";
+import ganz from "./ganz";
+import Loadable, { LoadableWrapper } from "./loadable";
 
 export type Verifiable<T> = Loadable<T> & { verified: boolean };
 
 export default Verifiable;
+
+//
+
+export class VerifiableWrapper<T, G extends Verifiable<T> = Verifiable<T>> extends LoadableWrapper<T, G> {
+
+    constructor(g: G) {
+        super(g);
+        this.g = g;
+    }
+
+    public verified(verified: boolean = true): this {
+        ganz.setVerified(this.g, verified);  
+        return this;
+    }
+}
