fa81d82266815ec7db7a64c78904d20814462d43
k4yr2 <kyrzkaya@gmail.com>
2025-06-13 20:28:38 +0300

    Add LoadableWrapper class to enhance type handling and loading functionality

--- src/loadable.ts
+++ src/loadable.ts
@@ -1,5 +1,20 @@
-import Content from "./content";
+import Content, { ContentWrapper } from "./content";
+import ganz from "./ganz";
 
 export type Loadable<T> = Content<T> & { loading: boolean }
 
 export default Loadable;
+
+//
+
+export class LoadableWrapper<T, G extends Loadable<T> = Loadable<T>> extends ContentWrapper<T, G> {
+
+    constructor(g: G) {
+        super(g);
+    }
+
+    public loading(loading: boolean = true) : this {
+        ganz.setLoading(this.g, loading);
+        return this;
+    }
+}
