26cc4f25e3aaf868cec8be6ff20ab3febdd999f7
k4yr2 <kyrzkaya@gmail.com>
2025-06-14 01:17:01 +0300

    Remove defaultMap and static methods from folgen class for simplification

--- src/folgen.ts
+++ src/folgen.ts
@@ -6,29 +6,8 @@ import { FolgenValue } from "./folgenValue";
 
 export class folgen {
 
-    static #defaultMap : Map<string, FolgenType> = new Map<string, FolgenType>([
-        ["loadable", { loading: false } as FolgenLoadable],
-        ["verifiable", { verified: false } as FolgenVerifiable]
-    ]);
-
     private constructor() {
     }
-
-    //
-
-    public static loadable<T extends number>(content: T) {
-        return { content, loading: false } as FolgenValue<T, FolgenLoadable>;
-    }
-
-    public static verifiable<T>(content: T) {
-        return { content, verified: false } as FolgenValue<T, FolgenVerifiable>;
-    }
-
-    public static union<T, U extends FolgenType[]>(content: T){
-        return { content } as FolgenValue<T, FolgenUnion<U>>;
-    }
-
-    //
 }
 
 export default folgen;
