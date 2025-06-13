4ed85571bb827c74f2af8fd0e1f214f566779032
k4yr2 <kyrzkaya@gmail.com>
2025-06-13 21:32:13 +0300

    Refactor FolgenVerifiable class: implement verified property and constructor

--- src/folgenVerifiable.ts
+++ src/folgenVerifiable.ts
@@ -1,5 +1,21 @@
 import FolgenLoadable from "./folgenLoadable";
 
-export type FolgenVerifiable<T> = FolgenLoadable<T> & { verified: boolean };
+export class FolgenVerifiable<T> extends FolgenLoadable<T> {
+    #verified: boolean = false;
+
+    public constructor(content: T | null) {
+        super(content);
+    }
+
+    public get verified(): boolean {
+        return this.#verified;
+    }
+
+    public set verified(value: boolean) {
+        if(this.#verified = value) {
+            this.loading = false;
+        }
+    }
+}
 
 export default FolgenVerifiable;
