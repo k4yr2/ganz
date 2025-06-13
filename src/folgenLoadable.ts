fb565dafaac5fb2b1c0920692f1a671bbbc1f052
k4yr2 <kyrzkaya@gmail.com>
2025-06-13 21:31:13 +0300

    Refactor FolgenLoadable class: extend FolgenBase and implement loading property

--- src/folgenLoadable.ts
+++ src/folgenLoadable.ts
@@ -1,5 +1,19 @@
-import FolgenContent from "./folgenContent";
+import { FolgenBase } from "./folgen";
 
-export type FolgenLoadable<T> = FolgenContent<T> & { loading: boolean }
+export class FolgenLoadable<T> extends FolgenBase<T> {
+    #loading: boolean = false;
+
+    public constructor(content: T | null) {
+        super(content);
+    }
+    
+    public get loading(): boolean {
+        return this.#loading;
+    }
+
+    public set loading(value: boolean) {
+        this.#loading = value;
+    }
+}
 
 export default FolgenLoadable;
